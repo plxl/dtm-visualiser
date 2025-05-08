f25e2d225a4f1c822175a0040218e06d420e2969
plxl <210124508+plxl@users.noreply.github.com>
2025-05-09 01:39:33 +0800

    ADD util functions for DTM visual playback

--- util.py
+++ util.py
@@ -12,3 +12,13 @@ def err(message: str):
 def err_popup(message: str):
     err(message)
     messagebox.showerror("Error", message)
+
+def ease_out_expo(t):
+    return 1 - pow(2, -10 * t) if t < 1 else 1
+
+def hex_to_rgb(h):
+        h = h.lstrip('#')
+        return tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+
+def rgb_to_hex(rgb):
+    return '#{:02x}{:02x}{:02x}'.format(*rgb)
